8cc2984e2d407896e27e2450158d81f1bcba52f4
Roselli <tomasrroselli@gmail.com>
2025-10-20 08:54:11 -0300

    cree clase ronda

---
+++ src/Modelo/Ronda.java
@@ -0,0 +1,42 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Ronda
+{
+    private String nombre;
+    ArrayList <Partido> partidos;
+
+    //falta completar metodo
+    public void jugarRonda(Equipo)
+    {
+
+    }
+
+    //falta +getGanadores(): Arraylist-Equipo-
+
+
+    //Constructor
+    public Ronda(String nombre, ArrayList<Partido> partidos) {
+        this.nombre = nombre;
+        this.partidos = partidos;
+    }
+
+    
+    //Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+}
