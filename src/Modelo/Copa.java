f0511f61c7e1f5b6e7259e8ec2bed89e9a066c1f
Juanson <juanmarzoni@gmail.com>
2025-10-20 09:17:17 -0300

    Comente clases copa, liga, temporada y torneo

--- src/Modelo/Copa.java
+++ src/Modelo/Copa.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 public class Copa extends Torneo{
     private ArrayList<Ronda> rondas;
 
+    //Constructor
     public Copa(String nombre) {
         super(nombre);
         this.rondas = new ArrayList<>();
     }
 
+    //Implementación del metodo jugar de la clase padre torneo
+    //Falta terminar
     @Override
     public void jugar() {
-        
+
     }
 }
