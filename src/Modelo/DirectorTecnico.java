01df46fa4807741d3eb67cfba3dc7027ee5196e5
nicolasferreyramdp-code <nicolasferreyramdp@gmail.com>
2025-10-20 19:29:33 -0300

    DirectorTecnico: Implementa serialización/deserialización JSON

--- src/Modelo/DirectorTecnico.java
+++ src/Modelo/DirectorTecnico.java
@@ -1,13 +1,35 @@
 package Modelo;
-
+import org.json.JSONObject;
 public class DirectorTecnico extends Persona{
     private int experiencia; //0 a 100
-    Tactica tacticaPreferida;
+    private Tactica tacticaPreferida;
 
     public DirectorTecnico(String nombre, int edad, String nacionalidad, int experiencia, Tactica tacticaPreferida) {
         super(nombre, edad, nacionalidad);
         this.experiencia = experiencia;
         this.tacticaPreferida = tacticaPreferida;
+    }
+
+    //==================== JSON ====================
+    public DirectorTecnico (JSONObject json){
+        //Clase padre
+        super(json.getString("nombre"), json.getInt("edad"), json.getString("nacionalidad"));
+
+        this.experiencia = json.getInt("experiencia");
+
+        JSONObject jsonDT = json.getJSONObject("tacticaPreferida");
+        this.tacticaPreferida = new Tactica(jsonDT);
+    }
+
+    public JSONObject toJson(){
+        JSONObject jsonDT = new JSONObject();
+        jsonDT.put("nombre", this.nombre);
+        jsonDT.put("edad", this.edad);
+        jsonDT.put("nacionalidad", this.nacionalidad);
+        jsonDT.put("experiencia", this.experiencia);
+        //llama al toJson() de Tactica
+        jsonDT.put("tacticaPreferida", this.tacticaPreferida.toJSON());
+        return jsonDT;
     }
 
     // ==================== Getters y Setters ====================
--- src/Modelo/Tactica.java
+++ src/Modelo/Tactica.java
@@ -1,15 +1,38 @@
 package Modelo;
 import enums.*;
 
+import org.json.JSONObject;
+
 public class Tactica {
-    Eformacion eformacion;
-    EstiloJuego estiloJuego;
+    private Eformacion eformacion;
+    private EstiloJuego estiloJuego;
 
     public Tactica(Eformacion eformacion, EstiloJuego estiloJuego) {
         this.eformacion = eformacion;
         this.estiloJuego = estiloJuego;
     }
 
+    //==================== JSON ====================
+    public Tactica (JSONObject json){
+        // Lee el valor String asociado a la clave "eformacion"
+        String formacionStr = json.getString("eformacion");
+        // Convierte el String de nuevo al tipo enum Eformacion
+        this.eformacion = Eformacion.valueOf(formacionStr);
+
+        // Lee el valor String asociado a la clave "estiloJuego"
+        String estiloStr = json.getString("estiloJuego");
+        // Convierte el String de nuevo al tipo enum EstiloJuego
+        this.estiloJuego = EstiloJuego.valueOf(estiloStr);
+    }
+
+    public JSONObject toJSON(){
+        JSONObject jsonTactica = new JSONObject();
+        jsonTactica.put("eformacion", eformacion.name());
+        jsonTactica.put("estiloJuego", estiloJuego.name());
+        return jsonTactica;
+    }
+
+    // ==================== Getters y Setters ====================
     public Eformacion getEformacion() {
         return eformacion;
     }
