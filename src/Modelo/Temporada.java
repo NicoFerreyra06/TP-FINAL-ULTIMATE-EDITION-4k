391901361cc5948b425443aff575cf2b1296672e
Juanson <juanmarzoni@gmail.com>
2025-10-20 08:58:06 -0300

    Cree la clase temporada

---
+++ src/Modelo/Temporada.java
@@ -0,0 +1,31 @@
+package Modelo;
+
+import java.util.HashMap;
+
+public class Temporada {
+    protected int anio;
+    protected Liga liga;
+    protected HashMap<String, Equipo> equipos;
+
+    public Temporada(int anio, Liga liga) {
+        this.anio = anio;
+        this.liga = liga;
+        this.equipos = new HashMap<>();
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public Liga getLiga() {
+        return liga;
+    }
+    public void setLiga(Liga liga) {
+        this.liga = liga;
+    }
+
+
+}
